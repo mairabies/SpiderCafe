8dce7c4feafe2f1ad58195489288b660d53ef3e1
mairabies <mairaathar1@gmail.com>
2023-03-28 10:55:58 -0500

    movement with shitty animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody rb;
+    private Rigidbody rb;
+    public CharacterController characterController;
+    public float speed = 5f;
+    private Animator anim;
+    public static int IDLE = 0;
+    public static int WALK = 1;
+    public static int ATTACK = 2;
+    public static int ATTACK2 = 3;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (dir.magnitude >= 0.1f)
+        {
+            anim.SetInteger("State", WALK);
+            characterController.Move(dir * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            anim.SetInteger("State", ATTACK);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            anim.SetInteger("State", ATTACK2);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+
     }
 }
