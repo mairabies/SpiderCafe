bbc7e1a3375bfe2b9724d2845ce1911f839f4906
mairabies <mairaathar1@gmail.com>
2023-04-03 11:18:19 -0500

    interactables psuedocode

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,13 +8,13 @@ public class Player : MonoBehaviour
     public float speed;
     public float turnSpeed;
     private Animator anim;
+    private bool interactable;
 
     private int state;
     public static int IDLE = 0;
     public static int WALK = 1;
     public static int ATTACK = 2;
     public static int ATTACK2 = 3;
-
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +27,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         float vertical = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
-        Vector3 dir = new Vector3(0f, vertical, 0f).normalized;
+        Vector3 dir = new Vector3(0f, 0f, vertical).normalized;
 
         if (dir.magnitude >= 0.1f)
         {
@@ -64,5 +64,27 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
 
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Interactable"))
+        {
+            Debug.Log("colliding");
+            interactable = true;
+
+            //have Interactions script and bool interactable inside. Set to true when collidiing, when true make "F to interact" variable visible
+            //Problem: how to choose which object to select when colliding with more than one
+            //interactable bool will also be condition for picking up and moving stuff
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Interactable"))
+        {
+            Debug.Log("exited collision");
+            interactable = false;
+        }
     }
 }
